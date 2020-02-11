18051519f7aa7ef4d58a7fa89cfdb0a63cd81e95
Dorian Young <DorianYoung@Ymail.com>
2020-02-11 01:25:54 -0800

    converted weather to fareignheight(sp?)

--- Assets/weatherDash.js
+++ Assets/weatherDash.js
@@ -13,12 +13,12 @@ function showCurrentWeather() {
     var textInput = $("#searchBar").val().toUpperCase();
     $.ajax({
         type: "POST",
-        url: "https://api.openweathermap.org/data/2.5/weather?q=" + textInput + "&appid=3c751c9c1e17ba49b5f5e5b15463bf0a&units=metric",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + textInput + "&appid=3c751c9c1e17ba49b5f5e5b15463bf0a&units=imperial",
         dataType: "json",
         success: function (result, status, xhr) {
             
             var currentWeatherTable = $("<table><tr><th><h2>" + result["name"] + "</h2></th></tr>");
-            currentWeatherTable.append("<tr><td>Current Temperature:</td><td>" + result["main"]["temp"] + "°C</td></tr>");
+            currentWeatherTable.append("<tr><td>Current Temperature:</td><td>" + result["main"]["temp"] + "°F</td></tr>");
             currentWeatherTable.append("<tr><td>Humidity:</td><td>" + result["main"]["humidity"] + "%</td></tr>");
             currentWeatherTable.append("<tr><td>Wind Speed:</td><td>" + result["wind"]["speed"] + "mph</td></tr>");
             currentWeatherTable.append("<tr><td>UV Index:</td><td>" + result["wind"]["speed"] + "</td></tr>");
@@ -49,42 +49,42 @@ function showForecast() {
     var textInput = $("#searchBar").val().toUpperCase();
     $.ajax({
         type: "POST",
-        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + textInput + "&appid=3c751c9c1e17ba49b5f5e5b15463bf0a&units=metric",
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + textInput + "&appid=3c751c9c1e17ba49b5f5e5b15463bf0a&units=imperial",
         dataType: "json",
         success: function (result, status, xhr) {
 
             //DAY 1
             var forecastTable = $("<table><tr><th>" + result["list"][0]["dt_txt"] +"</th></tr>");
             forecastTable.append("<tr><td> " + "<img id='icon' src='http://openweathermap.org/img/w/" + result["list"][0]["weather"][0]["icon"] + ".png' alt='weather icon'></img></td></tr>");
-            forecastTable.append("<tr><td>Temp: " + result["list"][0]["main"]["temp"] + "°C</td></tr>");
+            forecastTable.append("<tr><td>Temp: " + result["list"][0]["main"]["temp"] + "°F</td></tr>");
             forecastTable.append("<tr><td>Humidity: " + result["list"][0]["main"]["humidity"] + "%</td></tr>");
             $("#day1").html(forecastTable);
 
             //DAY 2
             var forecastTable = $("<table><tr><th>" + result["list"][8]["dt_txt"] +"</th></tr>");
             forecastTable.append("<tr><td> " + "<img id='icon' src='http://openweathermap.org/img/w/" + result["list"][8]["weather"][0]["icon"] + ".png' alt='weather icon'></img></td></tr>");
-            forecastTable.append("<tr><td>Temp: " + result["list"][8]["main"]["temp"] + "°C</td></tr>");
+            forecastTable.append("<tr><td>Temp: " + result["list"][8]["main"]["temp"] + "°F</td></tr>");
             forecastTable.append("<tr><td>Humidity: " + result["list"][8]["main"]["humidity"] + "%</td></tr>");
             $("#day2").html(forecastTable);
 
             //DAY 3
             var forecastTable = $("<table><tr><th>" + result["list"][16]["dt_txt"] +"</th></tr>");
             forecastTable.append("<tr><td> " + "<img id='icon' src='http://openweathermap.org/img/w/" + result["list"][16]["weather"][0]["icon"] + ".png' alt='weather icon'></img></td></tr>");
-            forecastTable.append("<tr><td>Temp: " + result["list"][16]["main"]["temp"] + "°C</td></tr>");
+            forecastTable.append("<tr><td>Temp: " + result["list"][16]["main"]["temp"] + "°F</td></tr>");
             forecastTable.append("<tr><td>Humidity: " + result["list"][16]["main"]["humidity"] + "%</td></tr>");
             $("#day3").html(forecastTable);
 
             //DAY 4
             var forecastTable = $("<table><tr><th>" + result["list"][24]["dt_txt"] +"</th></tr>");
             forecastTable.append("<tr><td> " + "<img id='icon' src='http://openweathermap.org/img/w/" + result["list"][24]["weather"][0]["icon"] + ".png' alt='weather icon'></img></td></tr>");
-            forecastTable.append("<tr><td>Temp: " + result["list"][24]["main"]["temp"] + "°C</td></tr>");
+            forecastTable.append("<tr><td>Temp: " + result["list"][24]["main"]["temp"] + "°F</td></tr>");
             forecastTable.append("<tr><td>Humidity: " + result["list"][24]["main"]["humidity"] + "%</td></tr>");
             $("#day4").html(forecastTable);
 
             //DAY 5
             var forecastTable = $("<table><tr><th>" + result["list"][32]["dt_txt"] +"</th></tr>");
             forecastTable.append("<tr><td> " + "<img id='icon' src='http://openweathermap.org/img/w/" + result["list"][32]["weather"][0]["icon"] + ".png' alt='weather icon'></img></td></tr>");
-            forecastTable.append("<tr><td>Temp: " + result["list"][32]["main"]["temp"] + "°C</td></tr>");
+            forecastTable.append("<tr><td>Temp: " + result["list"][32]["main"]["°F"] + "°F</td></tr>");
             forecastTable.append("<tr><td>Humidity: " + result["list"][32]["main"]["humidity"] + "%</td></tr>");
             $("#day5").html(forecastTable);
 
